ae2a9ea8932544dca8fad5beb4fa717d1fea7a4a
Carter-Grant <cartergrant100@comcast.net>
2024-10-29 09:36:14 -0600

    update hw 8

--- HW_8/easy.py
+++ HW_8/easy.py
@@ -10,4 +10,12 @@ def count_nodes(graph):
 #Example Data:
 g = nx.Graph()
 g.add_nodes_from([2, 4, 7, 9, 3])
-print(count_nodes(g))  # Output: 3
+print(count_nodes(g))
+
+
+#CHAT GPT Q: how do i get an example dataset for this code?
+#A:Creating a Simple Graph Manually You can create a graph manually by adding nodes and edges:
+#import networkx as nx
+# Create an empty graph
+#G = nx.Graph() # Add nodes
+#G.add_nodes_from([1, 2, 3, 4])
